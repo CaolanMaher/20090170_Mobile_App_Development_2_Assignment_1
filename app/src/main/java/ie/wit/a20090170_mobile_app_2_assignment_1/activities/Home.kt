e6d71d90926a49debc4c6c8cfa96d7ce252c2a2b
CaolanMaher <caolanmaher2@gmail.com>
2023-05-11 15:05:13 +0100

    Added Display User Image, Name And Email

--- app/src/main/java/ie/wit/a20090170_mobile_app_2_assignment_1/activities/Home.kt
+++ app/src/main/java/ie/wit/a20090170_mobile_app_2_assignment_1/activities/Home.kt
@@ -8,15 +8,20 @@ import androidx.navigation.*
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import ie.wit.a20090170_mobile_app_2_assignment_1.R
 import ie.wit.a20090170_mobile_app_2_assignment_1.databinding.HomeBinding
+import ie.wit.a20090170_mobile_app_2_assignment_1.databinding.NavHeaderBinding
+import ie.wit.a20090170_mobile_app_2_assignment_1.utils.customTransformation
 
 class Home : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var homeBinding : HomeBinding
+    private lateinit var navHeaderBinding : NavHeaderBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     private lateinit var auth: FirebaseAuth
@@ -54,8 +59,6 @@ class Home : AppCompatActivity() {
         val navView = homeBinding.navView
         navView.setupWithNavController(navController)
 
-
-
 //        navController.addOnDestinationChangedListener { _, destination, arguments ->
 //            when(destination.id) {
 //                R.id.reportFragment -> {
@@ -65,6 +68,32 @@ class Home : AppCompatActivity() {
 //                }
 //            }
 //        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        if(auth.currentUser != null) {
+            updateNavHeader(auth.currentUser!!)
+        }
+    }
+
+    private fun updateNavHeader(currentUser: FirebaseUser) {
+        var headerView = homeBinding.navView.getHeaderView(0)
+        navHeaderBinding = NavHeaderBinding.bind(headerView)
+        //navHeaderBinding.username.text = currentUser.displayName
+        navHeaderBinding.textView.text = currentUser.email
+
+        if(currentUser.photoUrl != null && currentUser.displayName != null) {
+            navHeaderBinding.username.text = currentUser.displayName
+
+            Picasso.get().load(auth.currentUser?.photoUrl)
+                .resize(200, 200)
+                .transform(customTransformation())
+                .centerCrop()
+                .into(navHeaderBinding.imageView)
+            //.into(navHeaderBinding.navHeaderImage)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/ie/wit/a20090170_mobile_app_2_assignment_1/utils/Helpers.kt
+++ app/src/main/java/ie/wit/a20090170_mobile_app_2_assignment_1/utils/Helpers.kt
@@ -1,4 +1,16 @@
 package ie.wit.a20090170_mobile_app_2_assignment_1.utils
 
-class Helpers {
-}
+import android.graphics.Color
+import com.google.firebase.auth.FirebaseAuth
+import com.makeramen.roundedimageview.RoundedTransformationBuilder
+import com.squareup.picasso.Transformation
+
+private lateinit var auth: FirebaseAuth
+
+fun customTransformation() : Transformation =
+    RoundedTransformationBuilder()
+        .borderColor(Color.WHITE)
+        .borderWidthDp(2F)
+        .cornerRadiusDp(35F)
+        .oval(false)
+        .build()
